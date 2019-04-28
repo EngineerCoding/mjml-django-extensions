e39c05f6d0494f07d51c3b7f94ac45f934f7ecf9
EngineerCoding <Engineercoding@hotmail.nl>
2019-04-28 13:32:55 +0200

    Add button compat

--- components/DjangoTrans.js
+++ components/DjangoTrans.js
@@ -8,6 +8,8 @@ registerDependencies({
     'mj-body': ['django-trans'],
     'mj-section': ['django-trans'],
     'mj-column': ['django-trans'],
+    'mj-button': ['django-trans'],
+    'django-block': ['django-trans'],
 });
 
 /**
